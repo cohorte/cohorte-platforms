8c01b38d802ed8755aac87fc1ac99d113444fe89
Thomas Calmant <thomas.calmant@isandlatech.com>
2014-10-24 12:54:04 +0200

    Added Java XMPP configuration for Herald

--- conf/herald/java-xmpp.js
+++ conf/herald/java-xmpp.js
@@ -6,24 +6,19 @@
 	 * Herald XMPP transport bundles
 	 */
 	"bundles" : [ {
-		"name" : "herald.transports.xmpp.directory"
-	}, {
-		"name" : "herald.transports.xmpp.transport"
+		"name" : "org.cohorte.herald.xmpp"
 	} ],
 
-	/*
-	 * Components
-	 */
 	"composition" : [ {
 		"factory" : "herald-xmpp-transport-factory",
 		"name" : "herald-xmpp-transport",
 		"properties" : {
 			"xmpp.server" : "localhost",
-			"xmpp.port" : 5222,
+			"xmpp.port" : "5222",
 			"xmpp.monitor.jid" : "bot@phenomtwo3000",
 			"xmpp.room.jid" : "cohorte@conference.phenomtwo3000",
 			// FIXME: Should be given by the forker
-			"xmpp.monitor.key" : 42
+			"xmpp.monitor.key" : "42"
 		}
 	} ]
 }
