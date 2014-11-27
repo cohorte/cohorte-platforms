d19d71d8404b8e49001d3352b6b9a99faee91d12
Thomas Calmant <thomas.calmant@gmail.com>
2014-11-27 12:09:40 +0100

    Added setup.bat and prepare.bat to support Windows deployment
    Execution script are not available yet.

---
+++ setup.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM Go to the Cohorte platform directory
+pushd %~dp0
+
+REM Set environment variables
+echo [INFO] Exporting the COHORTE_HOME environment variable...
+set COHORTE_HOME=%~dp0
+set PATH="%COHORTE_HOME%/bin";%PATH%
+echo [INFO] COHORTE_HOME=%COHORTE_HOME%
+
+REM Get back where we were
+popd
+
+echo [INFO] Done.
