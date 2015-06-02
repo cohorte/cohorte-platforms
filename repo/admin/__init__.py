a13ce56509e6e5151fd640ae7832d29931f6a1af
debbabi <dbassem@gmail.com>
2015-06-02 10:57:22 +0200

    Commiting /admin REST API (not yet fully implemented and not yet used by webadmin)

---
+++ repo/admin/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -- Content-Encoding: UTF-8 --
+"""
+COHORTE REST Admin API
+
+:author: Bassem Debbabi
+:license: Apache License v2
+"""
+
+# Documentation strings format
+__docformat__ = "restructuredtext en"
+
+# ------------------------------------------------------------------------------
+# Service specifications
