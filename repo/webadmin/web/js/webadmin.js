ace8ef6c4b3e54aa6ef1fa13b3ede226ed3cdde3
Rémi GATTAZ <remi.gattaz@gmail.com>
2015-07-28 09:10:47 +0200

    Add an html escaping function
    Escape html tags in log files

--- repo/webadmin/web/js/webadmin.js
+++ repo/webadmin/web/js/webadmin.js
@@ -148,6 +148,10 @@ function CloseModalBox(){
 	});
 }
 
+function escapeHTML(html) {
+	return html.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+}
+
 //////////////////////////////////////////////////////
 //////////////////////////////////////////////////////
 //
