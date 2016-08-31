2998e70da80e1463cebbd2df81b9e6c4eb2fc0b9
Bassem Debbabi <dbassem@gmail.com>
2016-08-31 15:21:38 +0200

    adding trusted-host parameter when installing from forge pip server

--- build/scripts/prepare.sh
+++ build/scripts/prepare.sh
@@ -49,10 +49,12 @@ PATH=$WORKSPACE/$VENV_NAME/bin:$PATH
 . $VENV_NAME/bin/activate
 
 # Install test and deployment tools
-pip install --force --upgrade --index-url=$INDEX_URL pip==1.5.6 setuptools #|| return 2
-pip install --upgrade --index-url=$INDEX_URL wheel #|| return 2
-pip install --upgrade --index-url=$INDEX_URL nose #|| return 2
-pip install --upgrade --index-url=$INDEX_URL devpi-client #|| return 2
+PIP_HOST=forge.cohorte-technologies.com
+#--trusted-host $PIP_HOST
+pip install --force --upgrade --index-url=$INDEX_URL --trusted-host=$PIP_HOST pip==1.5.6 setuptools  #|| return 2
+pip install --upgrade --index-url=$INDEX_URL --trusted-host=$PIP_HOST wheel #|| return 2
+pip install --upgrade --index-url=$INDEX_URL --trusted-host=$PIP_HOST nose #|| return 2
+pip install --upgrade --index-url=$INDEX_URL --trusted-host=$PIP_HOST devpi-client #|| return 2
 
 # install JPype
 # JPype1-py3==0.5.5.2
@@ -70,7 +72,7 @@ cp -r build/extra/windows/* repo
 fi
 
 # Install dependencies
-pip install --upgrade --index-url=$INDEX_URL -r build/scripts/requirements.txt
+pip install --upgrade --index-url=$INDEX_URL --trusted-host=$PIP_HOST -r build/scripts/requirements.txt
 
 # Copy dependencies to repo
 
