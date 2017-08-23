7257372a5bdf499fc90c2e187e0a5de52f010f0e
Bassem Debbabi <dbassem@gmail.com>
2017-08-23 16:57:20 +0200

    Adding Nexus scripts allowing the creation and deletion of raw repositories

--- p2/upload.sh
+++ p2/upload.sh
@@ -2,7 +2,7 @@
 
 USERNAME=$1
 PASSWORD=$2
-COHORTE_VERSION=$3
+REPO_NAME=$3
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -10,10 +10,10 @@ cd $DIR/target/repository
 
 for file in ./*
 do
-	curl -v --user "$USERNAME:$PASSWORD" --upload-file ${file} https://nrm.cohorte.tech/repository/cohorte-p2/${COHORTE_VERSION}/${file}
+	curl -v --user "$USERNAME:$PASSWORD" --upload-file ${file} https://nrm.cohorte.tech/repository/${REPO_NAME}/${file}
 done;
 
 for file in plugins/*
 do
-	curl -v --user "$USERNAME:$PASSWORD" --upload-file ${file} https://nrm.cohorte.tech/repository/cohorte-p2/${COHORTE_VERSION}/${file}
+	curl -v --user "$USERNAME:$PASSWORD" --upload-file ${file} https://nrm.cohorte.tech/repository/${REPO_NAME}/${file}
 done;
