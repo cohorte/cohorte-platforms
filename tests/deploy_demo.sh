f1bb5d19b8c4e9de279f5ff1f21f50b345d02943
Bassem Debbabi <dbassem@gmail.com>
2017-08-25 16:17:17 +0200

    Add missing reference to private key

--- tests/deploy_demo.sh
+++ tests/deploy_demo.sh
@@ -1,2 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ssh -o StrictHostKeyChecking=no root@api-dev.cohorte.tech $(cat $DIR/../docker/demo/start_instance.sh)
+ssh -o StrictHostKeyChecking=no -i $1 root@api-dev.cohorte.tech $(cat $DIR/../docker/demo/start_instance.sh)
