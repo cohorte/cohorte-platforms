4cb59cc9e016cef39625568044f739c756a13798
apisu <apisu@MBPdeAurelien2.office>
2017-10-03 12:11:09 +0200

    change cohorte boot file in order to run a .sh or a .py at startup with argument

--- cohorte-home/docker/node/install/boot.sh
+++ cohorte-home/docker/node/install/boot.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 echo "--- Boot of Cohorte Container..."
-./opt/init.sh
+#./opt/init.sh
+# check what kind of init we have. init.sh or init.py 
+
+if[ -f ./opt/init.sh ]; then
+	echo "run /opt/init.sh $@"
+	sh ./opt/init.sh "$@"
+elif [-f ./opt/init.py ]; then
+	echo "run python /opt/init.py $@"
+
+	python ./opt/init.py "$@"
+fi 
+
+
 ./usr/lib/systemd/systemd --system
