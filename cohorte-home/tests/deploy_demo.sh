814ecbbf918b6232496c31545b390c39775f6b4d
apisu <apisu@MBPdeAurelien2.office>
2017-11-09 17:15:29 +0100

    #103 add deploy and test for arm

--- cohorte-home/tests/deploy_demo.sh
+++ cohorte-home/tests/deploy_demo.sh
@@ -1,2 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "deploy on api-dev"
 ssh -o StrictHostKeyChecking=no -i $1 root@api-dev.cohorte.tech $(cat $DIR/../docker/demo/start_instance.sh)
+
+echo "deploy on api-dev-arm"
+ssh -o StrictHostKeyChecking=no -i $1 root@api-dev-arm.cohorte.tech $(cat $DIR/../docker/demo-arm64v8/start_instance.sh)
--- cohorte-home/tests/test_demo.sh
+++ cohorte-home/tests/test_demo.sh
@@ -1,2 +1,22 @@
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#docker run -v $DIR/collections:/etc/newman -t postman/newman_ubuntu1404:3.8.1 run CohortePlatform_Demo.postman_collection.json --disable-unicode --ignore-redirects
+
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -v $DIR/collections:/etc/newman -t postman/newman_ubuntu1404:3.8.1 run CohortePlatform_Demo.postman_collection.json --disable-unicode --ignore-redirects
+
+
+cd $DIR 
+
+COLLECTION_FILE_NAME=$1
+SERVER_HOST=$2
+
+echo "DIR=$DIR"
+echo "COLLECTION_FILE_NAME=$COLLECTION_FILE_NAME"
+echo "SERVER_HOST=$SERVER_HOST"
+
+
+sed  "s/{{host}}/$SERVER_HOST/g" collections/$COLLECTION_FILE_NAME > used_$COLLECTION_FILE_NAME
+docker run -v $DIR:/etc/newman -t postman/newman_ubuntu1404:3.8.1 run used_$COLLECTION_FILE_NAME --disable-unicode --ignore-redirects
+
+rm -f used_$COLLECTION_FILE_NAME
+
