b25bad48a3f18aa5d8915b21e13bf7e396b0494d
apisu <apisu@MBPdeAurelien2.office>
2018-02-16 09:57:08 +0100

    #108 remove override of node composition for demo

--- cohorte-home/docker/demo-devices/install/node/conf/herald/python-transport.js
+++
@@ -1,4 +0,0 @@
-/* WARNING!: do not edit, this file is generated automatically by COHORTE startup scripts. */
-{
-    "import-files" : [ "python-http.js" ]
-}
--- cohorte-home/docker/demo/install/node/conf/boot-common.js
+++
@@ -1,9 +0,0 @@
-/* WARNING!: do not edit, this file is generated automatically by COHORTE startup scripts. */
-{
-    "properties" : {
-        
-    "herald.application.id" : "demo",
-    "cohorte.node.data.dir" : "/Users/apisu/Documents/repositories/cohorte/cohorte-platforms/cohorte-home/docker/demo/install/node/data"
-
-    }
-}
