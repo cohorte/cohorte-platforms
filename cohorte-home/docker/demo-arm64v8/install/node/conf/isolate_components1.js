c14b87b72cd5eca3616ca023b35e4352b471e717
apisu <apisu@MBPdeAurelien2.office>
2018-02-19 15:30:16 +0100

    fix isolate factory

--- cohorte-home/docker/demo-arm64v8/install/node/conf/isolate_components1.js
+++ cohorte-home/docker/demo-arm64v8/install/node/conf/isolate_components1.js
@@ -4,6 +4,8 @@
 		 * Override the HTTP service configuration
 		 */		
 		"name" : "pelix-http-service",
+		"factory" : "pelix.http.service.basic.factory",
+
 		"properties" : {
 			"pelix.http.port" : 8091
 		}
--- cohorte-home/docker/demo-arm64v8/install/node/conf/isolate_components2.js
+++ cohorte-home/docker/demo-arm64v8/install/node/conf/isolate_components2.js
@@ -4,6 +4,7 @@
 		 * Override the HTTP service configuration
 		 */		
 		"name" : "pelix-http-service",
+		"factory" : "pelix.http.service.basic.factory",
 		"properties" : {
 			"pelix.http.port" : 8092
 		}
