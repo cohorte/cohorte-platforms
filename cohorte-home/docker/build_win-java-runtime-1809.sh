bb5830defb2c8b49ebd365a08d0dc54c8424a8fc
Aurelien PISU <apisu@MBP-de-admin-2.home>
2020-12-03 16:08:56 +0100

    don't continue to try to use python34 .use java runtime vs python runtime

---
+++ cohorte-home/docker/build_win-java-runtime-1809.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
+
+DIRECTORY="$DIR/runtime-java-win"
+DOCKER_REPOSITORY="cohorte/runtime"
+
+DOCKER_TAG=$1-java-win1809
+DOCKER_USER=$2
+DOCKER_PASSWORD=$3
+echo "DOCKER_TAG=$DOCKER_TAG"
+echo "DOCKER_USER=$DOCKER_USER"
+echo "DOCKER_PASSWORD=$DOCKER_PASSWORD"
+
+DOCKER_REGISTRY="dr.cohorte.tech"
+
+echo -e "\x1B[1;32m[INFO] Building Image [$DOCKER_REPOSITORY:$DOCKER_TAG] located on [$DIRECTORY]\x1B[0m"
+
+mkdir -p $DIR/runtime/install
+rm -rf $DIR/runtime/install/*
+
+cp $DIR/../target/cohorte-*-distribution.tar.gz $DIR/runtime/install/cohorte.tar.gz
+
+bash $DIR/build_image.sh "$DIRECTORY" "$DOCKER_REPOSITORY" "$DOCKER_TAG" Dockerfile "$DOCKER_USER" "$DOCKER_PASSWORD" "$DOCKER_REGISTRY"
