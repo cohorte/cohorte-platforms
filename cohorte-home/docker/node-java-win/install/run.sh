f930fd0ab53595a5dd7b37a94c6adefca622b2cc
Aurelien PISU <apisu@MBP-de-admin-2.home>
2020-12-04 12:22:55 +0100

    fix run.sh line 7

--- cohorte-home/docker/node-java-win/install/run.sh
+++ cohorte-home/docker/node-java-win/install/run.sh
@@ -4,24 +4,17 @@ echo "--- Boot of Cohorte Container..."
 # check what kind of init we have. init.sh or init.py 
 PWD=`pwd`
 echo "current dir $PWD"
-cd /opt
+cd ./opt
 
-if [ -f /opt/node/felix/launch/launch_jvm.sh ]; then
-	sh /opt/node/felix/launch/launch_jvm.sh
+if [ -f ./opt/node/felix/launch/launch_jvm.sh ]; then
+	sh ./opt/node/felix/launch/launch_jvm.sh
 fi
-
-# execute shell 
-echo "execute dependencies if we need a new dependency in the docker container"
-for deps in init_container_*.sh ; do
-	echo "call sh $deps $*"
-	sh $deps $*
-done
 
 
 echo "set handle sigterm/ sigkill"
 _term() { 
   echo "Caught SIGTERM signal! stop cohorte" 
-  /bin/echo "stop 0" -n | /bin/nc localhost 9001
+  echo "stop 0" -n | /bin/nc localhost 9001
 }
 
 trap _term SIGTERM
