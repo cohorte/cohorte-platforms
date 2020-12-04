b1bdf19da3bb25aa867d74c43da772c95c68835c
Aurelien PISU <apisu@MBP-de-admin-2.home>
2020-12-04 10:33:12 +0100

    add file for building runtime for windows

--- cohorte-home/docker/build_win-java-runtime-1909.sh
+++ cohorte-home/docker/build_win-java-runtime-1909.sh
@@ -6,7 +6,7 @@ echo $DIR
 DIRECTORY="$DIR/runtime-java-win"
 DOCKER_REPOSITORY="cohorte/runtime"
 
-DOCKER_TAG=$1-java-win1809
+DOCKER_TAG=$1-java-win1909
 DOCKER_USER=$2
 DOCKER_PASSWORD=$3
 echo "DOCKER_TAG=$DOCKER_TAG"
@@ -22,4 +22,4 @@ rm -rf $DIR/runtime/install/*
 
 cp $DIR/../target/cohorte-*-distribution.tar.gz $DIR/runtime/install/cohorte.tar.gz
 
-bash $DIR/build_image.sh "$DIRECTORY" "$DOCKER_REPOSITORY" "$DOCKER_TAG" Dockerfile-1809 "$DOCKER_USER" "$DOCKER_PASSWORD" "$DOCKER_REGISTRY"
+bash $DIR/build_image.sh "$DIRECTORY" "$DOCKER_REPOSITORY" "$DOCKER_TAG" Dockerfile-1909 "$DOCKER_USER" "$DOCKER_PASSWORD" "$DOCKER_REGISTRY"
