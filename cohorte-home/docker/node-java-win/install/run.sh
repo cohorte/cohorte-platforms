81f8248f9a3f9b920439efccb9fd98f094d7d937
Aurelien PISU <apisu@MBP-de-admin-2.home>
2020-12-04 12:36:21 +0100

    change path

--- cohorte-home/docker/node-java-win/install/run.sh
+++ cohorte-home/docker/node-java-win/install/run.sh
@@ -4,7 +4,7 @@ echo "--- Boot of Cohorte Container..."
 # check what kind of init we have. init.sh or init.py 
 PWD=`pwd`
 echo "current dir $PWD"
-cd ./opt
+cd /c/opt
 
 
 
@@ -17,13 +17,13 @@ _term() {
 trap _term SIGTERM
 
 echo "check launch_jvm.sh exists"
-if [ -f ./opt/node/felix/launch/launch_jvm.sh ]; then
+if [ -f /c/opt/node/felix/launch/launch_jvm.sh ]; then
 	echo "launch launch_jvm.sh "
 	echo "start cohorte" 
-	sh ./opt/node/felix/launch/launch_jvm.sh
+	sh ./c/opt/node/felix/launch/launch_jvm.sh
 else
-	echo "no file ./opt/node/felix/launch/launch_jvm.sh"
-	ls ./opt/node/felix/launch/
+	echo "no file /c/opt/node/felix/launch/launch_jvm.sh"
+	ls /c/opt/node/felix/launch/
 
 fi
 
