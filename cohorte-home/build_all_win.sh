8cf53f4ef01a0b3a1321d1938e48c97a9df037c0
Aurelien PISU <apisu@MBP-de-admin-2.home>
2021-03-09 16:25:27 +0100

    review build all win

--- cohorte-home/build_all_win.sh
+++ cohorte-home/build_all_win.sh
@@ -10,10 +10,11 @@ mvn deploy -U -Pfull
 
 mvn deploy -U -P generate_local_p2
 
-mvn deploy -U -P generate_update_site
+mvn install -P generate_docker_base_win_image
 
-mvn install -P generate_docker_runtime_image
+mvn install -P generate_docker_runtime_win_image
 
-mvn install -P generate_docker_node_image
+mvn install -P generate_docker_runtime_win_image
 
-mvn install -P generate_docker_demo_image
+mvn install -P generate_docker_node_win_image
+
